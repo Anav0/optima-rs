a499bd421c72f72e8df51068cbba0f0dddc3ed05
Igor <igor_motyka@mail.com>
2022-03-21 15:22:48 +0100

    Changed ParticalSwarm to be less generic

--- examples/swarm.rs
+++ examples/swarm.rs
@@ -1,109 +1,45 @@
 use optima_rust::{
     annealing::stop::MaxSteps,
     base::{Criterion, Evaluation, OptAlgorithm, Solution},
-    swarm::{ParticleSwarm, Point},
+    swarm::{Particle, ParticleSwarm},
 };
-use rand::{distributions::Uniform, prelude::Distribution, thread_rng, Rng};
+use rand::{
+    distributions::Uniform,
+    prelude::{Distribution, ThreadRng},
+    thread_rng, Rng,
+};
 
 fn main() {
     let min = -10.0;
     let max = 10.0;
 
-    let initial_sol = FunctionSolution::new(min, max);
-    let mut stop_criteria = MaxSteps::new(20000);
-    let mut swarm = ParticleSwarm::new(10, initial_sol, &mut stop_criteria);
+    let mut rng = thread_rng();
+    let uniform = Uniform::new_inclusive(min, max);
+    let mut stop_criteria = MaxSteps::new(50000);
+    let mut swarm = ParticleSwarm::new(10, min, max, &mut stop_criteria);
 
     //@Info: For this function best value is 2594 (when x and y is limited to range -10..10).
-    let mut criterion = Criterion::new(
-        &|_| 0.0,
-        &|sol: &FunctionSolution| booth(sol.x, sol.y),
-        false,
-    );
+    let mut criterion = Criterion::new(&|_| 0.0, &|part: &Particle| booth(part.x, part.y), true);
 
     let best = swarm.solve(&mut criterion);
     println!(
         "Best found: booth({},{} = {})",
         best.x,
         best.y,
-        best.get_eval().value
+        best.get_value()
     );
-}
-
-#[derive(Debug, Copy, Clone)]
-struct FunctionSolution {
-    eval: Evaluation,
-    min: f64,
-    max: f64,
-    x: f64,
-    y: f64,
-}
-impl FunctionSolution {
-    pub fn new(min: f64, max: f64) -> Self {
-        let mut rng = thread_rng();
-        let uniform = Uniform::new_inclusive(min, max);
-        Self {
-            eval: Evaluation::default(),
-            max,
-            min,
-            x: uniform.sample(&mut rng),
-            y: uniform.sample(&mut rng),
-        }
-    }
-}
-impl Solution for FunctionSolution {
-    fn get_eval(&self) -> &optima_rust::base::Evaluation {
-        &self.eval
-    }
-
-    fn get_eval_mut(&mut self) -> &mut optima_rust::base::Evaluation {
-        &mut self.eval
-    }
-}
-
-impl Point for FunctionSolution {
-    fn subtract(&self, other: &Self) -> f64 {
-        (self.x - other.x) + (self.y - self.y)
-    }
-
-    fn update(&mut self, velocity: f64) {
-        self.x += velocity;
-        self.y += velocity;
-
-        if self.x > self.max {
-            self.x = self.max;
-        }
-        if self.x < self.min {
-            self.x = self.min;
-        }
-
-        if self.y > self.max {
-            self.y = self.max;
-        }
-        if self.y < self.min {
-            self.y = self.min;
-        }
-    }
-
-    fn new_random(&self) -> Self {
-        let mut rng = thread_rng();
-        let uniform = Uniform::new_inclusive(self.min, self.max);
-
-        let mut eval = self.eval.clone();
-        eval.value = 0.0;
-        eval.is_feasible = false;
-
-        Self {
-            eval,
-            min: self.min,
-            max: self.max,
-            x: uniform.sample(&mut rng),
-            y: uniform.sample(&mut rng),
-        }
-    }
 }
 
 //Function to optimize. Booth function
 //https://en.wikipedia.org/wiki/Test_functions_for_optimization
 fn booth(x: f64, y: f64) -> f64 {
-    f64::powf(x + 2.0 * y - 7.2, 2.0) + f64::powf(2.0 * x + y - 5.2, 2.0)
+    f64::powf(x + 2.0 * y - 7.0, 2.0) + f64::powf(2.0 * x + y - 5.0, 2.0)
+}
+
+fn bukin(x: f64, y: f64) -> f64 {
+    100.0 * (y - 0.01 * x.powf(2.0)).sqrt() + 0.001 * (x + 10.0).abs()
+}
+
+fn cormick(x: f64, y: f64) -> f64 {
+    (x + y).sin() + (x - y).powf(2.0) - 1.5 * x + 2.5 * y + 1.0
 }
--- src/swarm.rs
+++ src/swarm.rs
@@ -1,47 +1,70 @@
-use std::str::ParseBoolError;
-
-use rand::{thread_rng, Rng};
+use rand::{
+    distributions::Uniform,
+    prelude::{Distribution, ThreadRng},
+    thread_rng, Rng,
+};
 
 use crate::{
     annealing::stop::StopCriteria,
-    base::{Criterion, OptAlgorithm, Solution},
+    base::{Criterion, Evaluation, OptAlgorithm, Solution},
 };
 
-pub trait Point {
-    fn subtract(&self, other: &Self) -> f64;
-    fn update(&mut self, velocity: f64);
-    fn new_random(&self) -> Self;
-}
-
-pub struct Particle<S> {
+#[derive(Clone)]
+pub struct Particle {
     best_local_index: usize,
-    velocity: f64,
-    solution: S,
+    velocity_x: f64,
+    velocity_y: f64,
+    pub x: f64,
+    pub y: f64,
+    eval: Evaluation,
 }
 
-impl<S> Particle<S>
-where
-    S: Solution + Point,
-{
+impl Particle {
     pub fn get_value(&self) -> f64 {
-        self.solution.get_eval().value
+        self.eval.value
+    }
+    pub fn update_position(&mut self, min: f64, max: f64) {
+        self.x += self.velocity_x;
+        self.y += self.velocity_y;
+
+        if self.x > max {
+            self.x = max;
+        }
+        if self.x < min {
+            self.x = min;
+        }
+
+        if self.y > max {
+            self.y = max;
+        }
+        if self.y < min {
+            self.y = min;
+        }
     }
 }
+impl Solution for Particle {
+    fn get_eval(&self) -> &Evaluation {
+        &self.eval
+    }
 
-pub struct ParticleSwarm<'a, S: Solution + Clone + Point> {
-    pub particles: Vec<Particle<S>>,
+    fn get_eval_mut(&mut self) -> &mut Evaluation {
+        &mut self.eval
+    }
+}
+pub struct ParticleSwarm<'a> {
+    pub particles: Vec<Particle>,
     best_global_index: usize,
     stop_criteria: &'a mut dyn StopCriteria,
-    initial_solution: S,
     local_attraction: f64,
     global_attraction: f64,
+    min: f64,
+    max: f64,
     inertia: f64,
+    rng: ThreadRng,
+    points_distribution: Uniform<f64>,
 }
 
-impl<'a, S> ParticleSwarm<'a, S>
-where
-    S: Solution + Clone + Point,
-{
+impl<'a> ParticleSwarm<'a> {
     fn is_better(&self, that: usize, this: usize, is_minimization: bool) -> bool {
         let best_value = self.particles[this].get_value();
         let current_value = self.particles[that].get_value();
@@ -58,30 +81,37 @@ where
         false
     }
 
-    pub fn new(size: usize, initial_solution: S, stop_criteria: &'a mut dyn StopCriteria) -> Self {
+    pub fn new(size: usize, min: f64, max: f64, stop_criteria: &'a mut dyn StopCriteria) -> Self {
+        let rng = thread_rng();
+        let distribution = Uniform::new_inclusive(min, max);
         Self {
             particles: Vec::with_capacity(size),
             best_global_index: 0,
-            initial_solution,
             stop_criteria,
             global_attraction: 0.5,
             local_attraction: 0.5,
             inertia: 0.05,
+            rng,
+            min,
+            max,
+            points_distribution: distribution,
         }
     }
-    fn initialize(&mut self, criterion: &mut Criterion<S>) {
+    fn initialize(&mut self, criterion: &mut Criterion<Particle>) {
         self.particles.clear();
 
-        let mut rng = thread_rng();
         for i in 0..self.particles.capacity() {
             // Pick random position in search space.
             let mut particle = Particle {
                 best_local_index: i,
-                solution: self.initial_solution.new_random(),
-                velocity: rng.gen(),
+                velocity_x: self.rng.gen(),
+                velocity_y: self.rng.gen(),
+                x: self.points_distribution.sample(&mut self.rng),
+                y: self.points_distribution.sample(&mut self.rng),
+                eval: Evaluation::default(),
             };
 
-            criterion.evaluate(&mut particle.solution);
+            criterion.evaluate(&mut particle);
             self.particles.push(particle);
 
             //If current particle is better then best save that info
@@ -92,44 +122,57 @@ where
     }
 }
 
-impl<'a, S> OptAlgorithm<'a, S> for ParticleSwarm<'a, S>
-where
-    S: Solution + Clone + Point,
-{
-    fn solve(&mut self, criterion: &mut Criterion<S>) -> S {
+impl<'a> OptAlgorithm<'a, Particle> for ParticleSwarm<'a> {
+    fn solve(&mut self, criterion: &mut Criterion<Particle>) -> Particle {
         self.initialize(criterion);
 
         let best_value = self.particles[self.best_global_index].get_value();
-        let mut rng = thread_rng();
+        let mut counter = 0;
         while !self.stop_criteria.should_stop(best_value) {
             for i in 0..self.particles.len() {
-                let particle = &self.particles[i];
                 //Pick random parameters r_i and r_g
-                let r_local: f64 = rng.gen();
-                let r_global: f64 = rng.gen();
+                let r_local: f64 = self.rng.gen();
+                let r_global: f64 = self.rng.gen();
 
-                //Update velocity
+                //Update x velocity
+                let particle = &self.particles[i];
                 let best_local = &self.particles[particle.best_local_index];
                 let best_global = &self.particles[self.best_global_index];
-                let local_pos_sub = best_local.solution.subtract(&particle.solution);
-                let global_pos_sub = best_global.solution.subtract(&particle.solution);
+                let local = self.local_attraction * r_local * (best_local.x - particle.x);
+                let global = self.global_attraction * r_global * (best_global.x - particle.x);
+                let mut particle = &mut self.particles[i];
+                particle.velocity_x = self.inertia * particle.velocity_x + local + global;
 
-                let local = self.local_attraction * r_local * local_pos_sub;
-                let global = self.global_attraction * r_global * global_pos_sub;
-
+                //Update y velocity
+                let particle = &self.particles[i];
+                let best_local = &self.particles[particle.best_local_index];
+                let best_global = &self.particles[self.best_global_index];
+                let local = self.local_attraction * r_local * (best_local.y - particle.y);
+                let global = self.global_attraction * r_global * (best_global.y - particle.y);
                 let particle = &mut self.particles[i];
-                particle.velocity = self.inertia * particle.velocity + local + global;
+                particle.velocity_y = self.inertia * particle.velocity_y + local + global;
 
                 //Update position in search space according to velocity
-                particle.solution.update(particle.velocity);
+                particle.update_position(self.min, self.max);
+
+                if counter < 2 {
+                    println!(
+                        "{} {} {} {}",
+                        particle.x, particle.y, particle.velocity_x, particle.velocity_y
+                    );
+                }
 
                 //Update best and local trackers
                 if self.is_better(i, self.best_global_index, criterion.is_minimization) {
                     self.best_global_index = i;
                 }
             }
+            if counter < 2 {
+                println!("----------");
+            }
+            counter += 1;
         }
         //@ Improvement: Do not clone
-        self.particles[self.best_global_index].solution.clone()
+        self.particles[self.best_global_index].clone()
     }
 }
