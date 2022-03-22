8cc5f3b191d1d656da93f19c041431d89d10975f
Igor <igor_motyka@mail.com>
2022-03-22 15:17:47 +0100

    Added crate with macros

---
+++ optima-macros/src/lib.rs
@@ -0,0 +1,56 @@
+#![crate_type = "proc-macro"]
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse::Parser, parse_macro_input, DeriveInput, Error};
+
+#[proc_macro_derive(Solution)]
+pub fn solution_derive(input: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+
+    // eprintln!("{:#?}", &ast);
+
+    let name = &ast.ident;
+    let trait_name = &ast.ident;
+
+    quote! {
+        use crate::base::{Evaluation, Solution};
+
+        impl Solution for #name {
+            fn get_eval(&self) -> &Evaluation{
+                &self.eval
+            }
+
+            fn get_eval_mut(&mut self) -> &mut Evaluation{
+                &mut self.eval
+            }
+        }
+    }
+    .into()
+}
+
+#[proc_macro_attribute]
+pub fn solution_attr(args: TokenStream, input: TokenStream) -> TokenStream {
+    let mut ast = parse_macro_input!(input as DeriveInput);
+    match &mut ast.data {
+        syn::Data::Struct(ref mut struct_data) => match &mut struct_data.fields {
+            syn::Fields::Named(fields) => {
+                fields.named.push(
+                    syn::Field::parse_named
+                        .parse2(quote! { eval: Evaluation })
+                        .unwrap(),
+                );
+                return quote! {
+                    #ast
+                }
+                .into();
+            }
+            _ => Error::new_spanned(&ast.ident, "Struct need to have at least empty body")
+                .to_compile_error()
+                .into(),
+        },
+        _ => Error::new_spanned(&ast.ident, "New fields can be added only to struct")
+            .to_compile_error()
+            .into(),
+    }
+}
--- optima-rust/src/lib.rs
+++ optima-rust/src/lib.rs
@@ -2,3 +2,4 @@ pub mod annealing;
 pub mod base;
 pub mod genetic;
 pub mod swarm;
+pub use optima_macros::{solution_attr, Solution};
